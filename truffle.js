dbee6e81186411905aa541dab1ecbc7913dc521b
mfwarren <matt.warren@gmail.com>
2018-02-28 10:54:24 -0500

    updates to truffle config

--- truffle.js
+++ truffle.js
@@ -5,13 +5,23 @@ module.exports = {
       "live": {
         network_id: 1,
         host: "127.0.0.1",
-        port: 8546   // Different than the default below
+        port: 8546,   // Different than the default below
+        gas: 4000000,
+        gasLimit: 6692819,
+        from: "0xc4f4d7ef579df808b8bd201345bbe30efa8a59c5"
+      },
+      "development": {
+        network_id: "*",
+        host: "127.0.0.1",
+        port: 7545,   // ganache
+        gas: 4700000
       },
       "test": {
-        network_id: 1,
+        network_id: 3,
         host: "127.0.0.1",
-        port: 8545,   // Different than the default below
-        gas: 4700000
+        port: 8545,   // default
+        gas: 4700000,
+        from: "0xc4f4d7ef579df808b8bd201345bbe30efa8a59c5"
       }
     },
     rpc: {
